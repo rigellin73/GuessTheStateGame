cc3ad091ae74e2b0b1d538de337f8ccb517b5fdd
rigellin73 <rigellin73@gmail.com>
2025-02-03 14:16:43 +0100

    create method add_state to write state name on the map using state name and coordinates

--- states_map.py
+++ states_map.py
@@ -1,9 +1,16 @@
-import turtle
 from turtle import Turtle
 
 MAP_SHAPE = "blank_states_img.gif"
+ALIGN = "center"
+FONT = ("Arial", 12, "normal")
 
-class StatesMap(Turtle):
+class StatesMap:
     def __init__(self):
-        super().__init__()
-        self.shape(MAP_SHAPE)
+        self.s_map = Turtle(MAP_SHAPE)
+        self.s_name = Turtle()
+        self.s_name.hideturtle()
+        self.s_name.penup()
+
+    def add_state(self, state_name, x, y):
+        self.s_name.goto(x, y)
+        self.s_name.write(state_name, False, ALIGN, FONT)
